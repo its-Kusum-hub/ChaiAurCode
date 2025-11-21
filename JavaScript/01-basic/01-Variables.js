5f5ed2eaa7aaff4c374ed8ab9abc8b5353294838
Kusum-hub <kusumlatamurmu124@gmail.com>
2025-11-21 12:31:36 +0000

    git commit -m "Add 01-Variables.js"

---
+++ JavaScript/01-basic/01-Variables.js
@@ -0,0 +1,15 @@
+const accountId = 144553;
+let accountemail = "Kusum@123gmail.com";
+var accountPassword = "1234";
+accountCity = "jaipur";
+
+//accountId = 2; //not allowed
+console.log(accountId);
+/* Prefer not to use var because of issue in block scope and function scope */
+
+accountEmail = "abc@gmail.com";
+accountPassword = "0032";
+accountCity = "bokaro";
+
+console.log("store multiple tables");
+console.table([accountemail, accountPassword, accountCity]); // to print in table format
